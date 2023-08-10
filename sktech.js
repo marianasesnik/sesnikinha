345a7c1c47af87c88b2a0321cd3250a41a4011ab
marianasesnik <140711925+marianasesnik@users.noreply.github.com>
2023-08-10 10:45:50 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,14 @@
+unction setup() {
+    createCanvas(600, 600);
+    background(''black'');
+  }
+  
+  function draw(){
+    stroke(blue)
+    fill(red)
+    
+    1f(mousePressed)
+    rect(mouseX, movedX 20 35)
+    {
+      
+       }
